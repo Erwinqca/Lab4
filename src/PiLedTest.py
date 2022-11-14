6550a62b014267857ba85d8964acbd2f2c82a303
Erwin Quek <erwinquekca.21@ichat.sp.edu.sg>
2022-11-14 23:11:33 +0800

    Version 1

---
+++ src/PiLedTest.py
@@ -0,0 +1,36 @@
+from hal import hal_led as led
+from hal import hal_input_switch as switch
+import time
+import time
+switch.init()
+led.init()
+def blink_led(delay):
+    # Led Blink
+    led.set_output(0, 1)
+    time.sleep(delay)
+
+    led.set_output(0, 0)
+    time.sleep(delay)
+
+
+def main():
+    x = 0
+    led.init()
+    switch.init()
+
+    while True:
+        if switch.read_slide_switch() == 1:
+            blink_led(0.2)
+            x = 0
+
+        else:
+            # 25 x 0.2 = 5seconds
+            while x < 26:
+                blink_led(0.1)
+                led.set_output(0, 0)
+                x += 1
+
+
+# Main entry point
+if __name__ == "__main__":
+    main()
